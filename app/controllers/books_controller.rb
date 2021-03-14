51888f160c7e69c5038d6e26a03a9fecd7ab5e7a
sumiship <osumi.k.osumi.k@gmail.com>
2021-03-14 07:39:53 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,49 @@
 class BooksController < ApplicationController
   def top
   end
-  
+
   def index
+    @book=Book.new
+    @books=Book.all
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to("/books/#{@book.id}")
+    else
+      @books = Book.all
+      render action: :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path
+    else
+      render action: :edit
+    end
+  end
+
+
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
